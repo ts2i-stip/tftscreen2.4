e80a1d933c8e7fa2ee33cf6357630753916d84dd
R. ALOUI <aloui.radhouane@gmail.com>
2023-10-12 12:38:54 +0100

    Source code cleaning

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,3 @@
-// IMPORTANT: Adafruit_TFTLCD LIBRARY MUST BE SPECIFICALLY
-// CONFIGURED FOR EITHER THE TFT SHIELD OR THE BREAKOUT BOARD.
-// SEE RELEVANT COMMENTS IN Adafruit_TFTLCD.h FOR SETUP.
-
-// Modified for SPFD5408 Library by Joao Lopes
-// Version 0.9.2 - Rotation for Mega and screen initial
-
 // *** SPFD5408 change -- Begin
 #include <SPFD5408_Adafruit_GFX.h>    // Core graphics library
 #include <SPFD5408_Adafruit_TFTLCD.h> // Hardware-specific library
@@ -23,43 +16,12 @@
 
 #define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
 
-// When using the BREAKOUT BOARD only, use these 8 data lines to the LCD:
-// For the Arduino Uno, Duemilanove, Diecimila, etc.:
-//   D0 connects to digital pin 8  (Notice these are
-//   D1 connects to digital pin 9   NOT in order!)
-//   D2 connects to digital pin 2
-//   D3 connects to digital pin 3
-//   D4 connects to digital pin 4
-//   D5 connects to digital pin 5
-//   D6 connects to digital pin 6
-//   D7 connects to digital pin 7
-// For the Arduino Mega, use digital pins 22 through 29
-// (on the 2-row header at the end of the board).
-
-
-
+// tft declaration
 Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
-// If using the shield, all control and data lines are fixed, and
-// a simpler declaration can optionally be used:
-// Adafruit_TFTLCD tft;
-
-
-// Copy string from flash to serial port
-// Source string MUST be inside a PSTR() declaration!
-void progmemPrint(const char *str) {
-  char c;
-  while(c = pgm_read_byte(str++)) Serial.print(c);
-}
-
-// Same as above, with trailing newline
-void progmemPrintln(const char *str) {
-  progmemPrint(str);
-  Serial.println();
-}
 
 void setup(void){
   Serial.begin(115200);
-  progmemPrintln(PSTR("TFT LCD test"));
+  Serial.println(F("TFT LCD test"));
 
   tft.reset();
   tft.begin(0x9341); // SDFP5408
@@ -72,20 +34,19 @@ void setup(void){
         {0, 0, 0, 0});
   RectwithText t1("T1", 2, {20, 0, 1, 5}, {0, 0, 0, 0});
   RectwithText t2("T2", 2, {20, tft.width() - ts2i.getWidth(), 1, 5}, {0, 0, 0, 0});
-  RectwithText t1val("00.0", 4, {50, 0, 1, 5}, {0, 0, 0, 0});
-  RectwithText t2val("00.0", 4, {50, tft.width() - t1val.getWidth(), 1, 5}, {0, 0, 0, 0});
+  RectwithText t1val("9999", 4, {50, 0, 1, 5}, {0, 0, 0, 0});
+  RectwithText t2val("9999", 4, {50, tft.width() - t1val.getWidth(), 1, 5}, {0, 0, 0, 0});
+  RectwithText ok("9999", 4, {90, 0, 1, 5}, {0, 0, 0, 0});
+  RectwithText nok("9999", 4, {90, tft.width() - ok.getWidth(), 1, 5}, {0, 0, 0, 0});
   ts2i.draw();
   stib.draw();
   t1.draw();
   t2.draw();
   t1val.draw();
   t2val.draw();
-  delay(1000);
-  t1val.setText(String(11.1, 1));
-  t1val.draw();
-  //stib.draw();
-  
-  //tft.println("TS2I");
+  ok.draw();
+  nok.draw();
+  //t1val.setText(String(11.1, 1));
 }
 
 void loop(void){

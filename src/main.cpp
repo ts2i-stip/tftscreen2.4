aa6f45173e09b0a43921c4c254864dfbfe437346
R. ALOUI <aloui.radhouane@gmail.com>
2023-10-18 17:05:00 +0100

    Initial Fathi program

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,33 @@
+#include <Arduino.h>
+#include <Encoder.h>
 // *** SPFD5408 change -- Begin
 #include <SPFD5408_Adafruit_GFX.h>    // Core graphics library
 #include <SPFD5408_Adafruit_TFTLCD.h> // Hardware-specific library
 #include <SPFD5408_TouchScreen.h>
 // *** SPFD5408 change -- End
 #include <RectwithText.hpp>
-#include <string.h>
+// #include <string.h>
+// Définir les broches de l'encodeur
+Encoder monEncodeur1(20, 21); // Broches A et B de l'encodeur
+Encoder monEncodeur2(19, 18); // Broches A et B de l'encodeur
+// Start buttons
+#define boutonMarche1Pin 32
+#define boutonMarche2Pin 34
+// Cylinder inputs / outputs
+#define CYL_CNT_WIDTH 33
+#define SEN_CYL_CNT_WIDTH 30
+#define CYL_CNT_HOLE 37
+#define SEN_CYL_CNT_HOLE 28
+#define CYL_CNT_OPENING 31
+#define SEN_CYL_CNT_OPENING 36
+#define CYL_CNT_MARK 35
+#define SEN_CYL_CNT_MARK 26
+#define CYL_OPEN_EXIT 27
+
+int totalCounter1 = 0;
+int totalCounter2 = 0;
+long nbBonnes = 0;
+long nbMauvaises = 0;
 
 // The control pins for the LCD can be assigned to any digital or
 // analog pins...but we'll use the analog pins as this allows us to
@@ -18,37 +41,230 @@
 
 // tft declaration
 Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+RectwithText ts2i("TS2I", 2, {0, 0, 1, 5});
+RectwithText stib("STIB", 2, {0, 240, 1, 5});
+RectwithText t1("T1", 2, {20, 10, 1, 5});
+RectwithText t2("T2", 2, {20, 235, 1, 5});
+RectwithText t1val("000", 4, {60, 0, 1, 5}, {0, 0, 0, 0});
+RectwithText t2val("000", 4, {60, 230, 1, 5}, {0, 0, 0, 0});
+RectwithText ok("000", 4, {120, 0, 1, 5}, {0, 0, 0, 0});
+RectwithText nok("000", 4, {120, 230, 1, 5}, {0, 0, 0, 0});
 
-void setup(void){
-  Serial.begin(115200);
+// Valeurs minimales et maximales pour la course (à personnaliser)
+long valeurMin1 = 1397;
+long valeurMax1 = 1420;
+long valeurMin2 = 640;
+long valeurMax2 = 2000;
+
+bool is2ButtonsPressed()
+{
+  return digitalRead(boutonMarche1Pin) + digitalRead(boutonMarche1Pin) == 0;
+}
+
+void setup()
+{
+  Serial.begin(9600);
   Serial.println(F("TFT LCD test"));
+  pinMode(boutonMarche1Pin, INPUT_PULLUP);
+  pinMode(boutonMarche2Pin, INPUT_PULLUP);
+  pinMode(SEN_CYL_CNT_WIDTH, INPUT_PULLUP);
+  pinMode(SEN_CYL_CNT_HOLE, INPUT_PULLUP);
+  pinMode(SEN_CYL_CNT_OPENING, INPUT_PULLUP);
+  pinMode(SEN_CYL_CNT_MARK, INPUT_PULLUP);
+  pinMode(CYL_CNT_WIDTH, OUTPUT);
+  pinMode(CYL_CNT_HOLE, OUTPUT);
+  pinMode(CYL_CNT_OPENING, OUTPUT);
+  pinMode(CYL_CNT_MARK, OUTPUT);
+  pinMode(CYL_OPEN_EXIT, OUTPUT);
 
   tft.reset();
   tft.begin(0x9341); // SDFP5408
 
-  tft.setRotation(3);
+  tft.setRotation(1);
   tft.fillScreen(BLACK);
   tft.setCursor(0, 0);
-  RectwithText ts2i("TS2I", 2, {0, 0, 1, 5}, {0, 0, 0, 0});
-  RectwithText stib("STIB", 2, {0, tft.width() - ts2i.getWidth(), 1, 5},
-        {0, 0, 0, 0});
-  RectwithText t1("T1", 2, {20, 0, 1, 5}, {0, 0, 0, 0});
-  RectwithText t2("T2", 2, {20, tft.width() - ts2i.getWidth(), 1, 5}, {0, 0, 0, 0});
-  RectwithText t1val("9999", 4, {50, 0, 1, 5}, {0, 0, 0, 0});
-  RectwithText t2val("9999", 4, {50, tft.width() - t1val.getWidth(), 1, 5}, {0, 0, 0, 0});
-  RectwithText ok("9999", 4, {90, 0, 1, 5}, {0, 0, 0, 0});
-  RectwithText nok("9999", 4, {90, tft.width() - ok.getWidth(), 1, 5}, {0, 0, 0, 0});
+
   ts2i.draw();
   stib.draw();
   t1.draw();
   t2.draw();
   t1val.draw();
   t2val.draw();
+  ok.setBackgroudColor(GREEN);
   ok.draw();
   nok.draw();
-  //t1val.setText(String(11.1, 1));
 }
 
-void loop(void){
+void loop()
+{
+  bool boutonMarche1Presse = digitalRead(boutonMarche1Pin) == LOW;
+  bool boutonMarche2Presse = digitalRead(boutonMarche2Pin) == LOW;
+  if (is2ButtonsPressed())
+  {
+    // Les deux boutons "Marche 1" et "Marche 2" sont pressés simultanément
+    Serial.println("Départ cycle, sortie vérin épaisseur");
+    digitalWrite(CYL_CNT_WIDTH, HIGH); // Active le vérin
+    while (digitalRead(SEN_CYL_CNT_WIDTH) == LOW)
+    {
+      // Attendez
+    }
+    delay(3000);                          // Attend 2 secondes
+    Serial.println("---->fin sortie vérin épaisseur");
+    int position1 = monEncodeur1.read(); // Lire la position de l'encodeur
+    Serial.print("Position de l'encodeur1 : ");
+    Serial.println(position1);
+    Serial.print("Conversion num 2 chaine : ");
+    char mystr[7];
+    sprintf(mystr, "%d", position1);
+    Serial.println(mystr);
+    // Serial.println(String(position1, 0));
+    t1val.setText(mystr);
+    t1val.draw();
+    delay(1000);
+    position1 = 1400;
+    if (position1 >= valeurMin1 && position1 <= valeurMax1)
+    {
+      Serial.println("TEST 1 OK, sortie vérin ouverture");
+      // Ajoutez ici les actions pour le cas "Pièce bonne"
 
+      digitalWrite(CYL_CNT_HOLE, HIGH); // Active le vérin
+      while (digitalRead(SEN_CYL_CNT_HOLE) == LOW)
+        ;
+      delay(3000);                          // Attend 2 secondes
+      Serial.println("----> Fin sortie vérin ouverture");
+      long position2 = monEncodeur2.read(); // Lire la position de l'encodeur
+      Serial.print("Position de l'encodeur2 : ");
+      Serial.println(position2);
+      sprintf(mystr, "%d", position2);
+      // Serial.println(String(position1, 0));
+      t2val.setText(mystr);
+      t2val.draw();
+      delay(200);
+      position2 = 1000;
+      if (position2 >= valeurMin2 && position2 <= valeurMax2)
+      {
+        Serial.println("TEST 2 OK, sortie vérin marquage");
+        // Ajoutez ici les actions pour le cas "Pièce bonne"
+
+        digitalWrite(CYL_CNT_MARK, HIGH); // Active le vérin
+        while (digitalRead(SEN_CYL_CNT_MARK) == LOW)
+        {
+          // Attendez
+        }
+
+        delay(500);
+        Serial.println("Fin sortie vérin marquage");
+
+        digitalWrite(CYL_CNT_MARK, LOW); // Active le vérin
+        while (digitalRead(SEN_CYL_CNT_MARK) == HIGH)
+        {
+          // Attendez
+        }
+        // Attend 2 secondes
+        Serial.println(F("Basculer l'exit sur pieces bonnes"));
+        digitalWrite(CYL_OPEN_EXIT, HIGH); // Active le vérin
+
+        Serial.println(F("---->Retour vérin ouverture"));
+        digitalWrite(CYL_CNT_HOLE, LOW); // Active le vérin
+        while (digitalRead(SEN_CYL_CNT_HOLE) == HIGH)
+        {
+          // Attendez
+        }
+
+        Serial.println(F("---->Retour vérin épaisseur"));
+        digitalWrite(CYL_CNT_WIDTH, LOW); // Active le vérin
+        while (digitalRead(SEN_CYL_CNT_WIDTH) == HIGH)
+        {
+          // Attendez
+        }
+        // Attend 2 secondes
+        Serial.println(F("Ouverture porte"));
+        digitalWrite(CYL_CNT_OPENING, HIGH); // Active le vérin
+        while (digitalRead(SEN_CYL_CNT_OPENING) == HIGH)
+        {
+          // Attendez
+        }
+        delay(100);
+        
+        // Attend 2 secondes
+        Serial.println(F("----> Fermeture porte"));
+        digitalWrite(CYL_CNT_OPENING, LOW); // Active le vérin
+        while (digitalRead(SEN_CYL_CNT_OPENING) == LOW)
+        {
+          // Attendez
+        }
+        Serial.println(F("----> Basculer l'exit sur pieces mauvaises"));
+        digitalWrite(CYL_OPEN_EXIT, LOW); // Active le vérin
+        delay(800);
+        nbBonnes++;
+        char mystr[7];
+        sprintf(mystr, "%03d", nbBonnes);
+        ok.setText(mystr);
+        ok.draw();
+      }
+      else
+      {
+        Serial.println("TEST 2 NOK");
+        // Ajoutez ici les actions pour le cas "Pièce mauvaise"
+
+        digitalWrite(CYL_CNT_HOLE, LOW); // Active le vérin
+        while (digitalRead(SEN_CYL_CNT_HOLE) == HIGH)
+        {
+          // Attendez
+        }
+
+        digitalWrite(CYL_CNT_WIDTH, LOW); // Active le vérin
+        while (digitalRead(SEN_CYL_CNT_WIDTH) == HIGH)
+        {
+          // Attendez
+        }
+
+        digitalWrite(CYL_CNT_OPENING, HIGH); // Active le vérin
+        while (digitalRead(SEN_CYL_CNT_OPENING) == HIGH)
+        {
+          // Attendez
+        }
+
+        digitalWrite(CYL_CNT_OPENING, LOW); // Active le vérin
+        while (digitalRead(SEN_CYL_CNT_OPENING) == LOW)
+        {
+          // Attendez
+        } // Attend 2 secondes
+        nbMauvaises++;
+        char mystr[7];
+        sprintf(mystr, "%03d", nbMauvaises);
+        nok.setText(mystr);
+        nok.draw();
+      }
+    }
+    else
+    {
+      Serial.println("TEST 1 NOK");
+      // Ajoutez ici les actions pour le cas "Pièce mauvaise"
+
+      digitalWrite(CYL_CNT_WIDTH, LOW); // Active le vérin
+      while (digitalRead(SEN_CYL_CNT_WIDTH) == HIGH)
+      {
+        // Attendez
+      }
+
+      digitalWrite(CYL_CNT_OPENING, HIGH); // Active le vérin
+      while (digitalRead(SEN_CYL_CNT_OPENING) == HIGH)
+      {
+        // Attendez
+      }
+
+      digitalWrite(CYL_CNT_OPENING, LOW); // Active le vérin
+      while (digitalRead(SEN_CYL_CNT_OPENING) == LOW)
+      {
+        // Attendez
+      }
+      nbMauvaises++;
+      char mystr[7];
+      sprintf(mystr, "%03d", nbMauvaises);
+      nok.setText(mystr);
+      nok.draw();
+    }
+  }
+  delay(100); // Rafraîchir toutes les 100 ms (ajustez selon vos besoins)
 }
